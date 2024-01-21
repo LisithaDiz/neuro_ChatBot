8dd7b41e85f8c407df685a39d600f6e7a32352f6
Lisitha Dissanayaka <114801465+LisithaDiz@users.noreply.github.com>
2024-01-21 15:38:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from dotenv import load_dotenv
 import pickle
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.llms import OpenAI
@@ -23,30 +25,55 @@ with st.sidebar:
 
 
 def main():
+
     load_dotenv()
     st.header("Ask about Neuroscience 💬")
 
+    # upload a PDF file
+    # pdf = st.file_uploader("Upload your PDF", type='pdf')
     pdf_path = "Neuro.pdf"
-    store_name = os.path.basename(pdf_path)[:-4]
 
-    if os.path.exists(f"{store_name}.pkl"):
-        with open(f"{store_name}.pkl", "rb") as f:
-            VectorStore = pickle.load(f)
-    else:
-        st.error(f"Pickle file '{store_name}.pkl' not found. Please make sure the file exists.")
+    # Continue with your existing PDF processing code
+    pdf_reader = PdfReader(pdf_path)
 
-    # Accept user questions/query
-    query = st.text_input("Ask questions about your PDF file:")
+    # st.write(pdf)
+    if pdf_reader is not None:
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
 
-    if query:
-        docs = VectorStore.similarity_search(query=query, k=3)
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text=text)
 
-        llm = OpenAI(model_name='gpt-3.5-turbo')
-        chain = load_qa_chain(llm=llm, chain_type="stuff")
-        with get_openai_callback() as cb:
-            response = chain.run(input_documents=docs, question=query)
-            print(cb)
-        st.write(response)
+        # Extract the filename from the path
+        store_name = os.path.basename(pdf_path)[:-4]
+        st.write(f'{store_name}')
+
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pkl", "rb") as f:
+                VectorStore = pickle.load(f)
+        else:
+            embeddings = OpenAIEmbeddings()
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+            with open(f"{store_name}.pkl", "wb") as f:
+                pickle.dump(VectorStore, f)
+
+        # Accept user questions/query
+        query = st.text_input("Ask questions about your PDF file:")
+
+        if query:
+            docs = VectorStore.similarity_search(query=query, k=3)
+
+            llm = OpenAI(model_name='gpt-3.5-turbo')
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query)
+                print(cb)
+            st.write(response)
 
 
 if __name__ == '__main__':
